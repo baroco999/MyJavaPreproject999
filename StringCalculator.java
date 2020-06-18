87ab4b5ca8d73c057a9baaa9a23c00a5d5811ec4
baroco999 <56413398+baroco999@users.noreply.github.com>
2020-06-18 14:14:10 +0300

    Update StringCalculator.java

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,21 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class StringCalculator {
-   
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
-        while (sc.hasNext()) {
-            sb.append(sc.next());
+        char[] chars = sc.nextLine().toCharArray();
+        for (char ch : chars) {
+            if (!Character.isWhitespace(ch)) sb.append(ch);
         }
         StringAnalyzer sa = new StringAnalyzer(new String(sb));
         try {
             sa.compute();
-		} catch (NumberFormatException e) {
-	        System.out.println(e.getMessage());
-		}
+        } catch (NumberFormatException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            sc.close();
+        }
         System.out.println(sa.toString());
-        sc.close();
     }
 }
 
@@ -45,8 +47,8 @@ class StringAnalyzer {
         } else {
             s2 = strArray[3];
         }
-        
-        
+
+
         if (num == 0) {
             result = new StC().go(s1, s2, sign);
         } else {
@@ -63,59 +65,59 @@ class StC {
     StringBuilder r;
     StringBuffer t1;
     StringBuffer t2;
-    
+
     String go(String s1, String s2, char sign) throws NumberFormatException {
-            r = new StringBuilder();
-            r.append('\"');
-            t1 = new StringBuffer(s1);
-            t2 = new StringBuffer(s2);
-            if (t1.length() > 10) t1.delete(10, t1.length());
-            if (t2.length() > 10) t2.delete(10, t2.length());
-            switch (sign) {
-                case '+':
-                    r.append(t1).append(t2);
-                    break;
-                case '-':
-                    if (s1.endsWith(s2)) {
-                        r.append(s1.substring(0, (s1.length() - s2.length())));
-                    } else {
-                        r.append(t1);
-                    }
-                    break;
-                default: throw new NumberFormatException("Unsupported operation");
-            }
-            r.append('\"');
-            if (r.length() > 40) {
-                r.delete(40, r.length());
-                r.append(new char[]{'.', '.', '.'});
-            }
-            return new String (r);
+        r = new StringBuilder();
+        r.append('\"');
+        t1 = new StringBuffer(s1);
+        t2 = new StringBuffer(s2);
+        if (t1.length() > 10) t1.delete(10, t1.length());
+        if (t2.length() > 10) t2.delete(10, t2.length());
+        switch (sign) {
+            case '+':
+                r.append(t1).append(t2);
+                break;
+            case '-':
+                if (s1.endsWith(s2)) {
+                    r.append(s1.substring(0, (s1.length() - s2.length())));
+                } else {
+                    r.append(t1);
+                }
+                break;
+            default: throw new NumberFormatException("Unsupported operation");
+        }
+        r.append('\"');
+        if (r.length() > 40) {
+            r.delete(40, r.length());
+            r.append(new char[]{'.', '.', '.'});
+        }
+        return new String (r);
     }
 
     String go(String s1, int num, char sign) throws NumberFormatException {
-            r = new StringBuilder();
-            r.append('\"');
-            t1 = new StringBuffer(s1);
-            if (t1.length() > 10) t1.delete(10, t1.length());
-            switch (sign) {
-                case '*':
-                    int i = 1;
-                    while (i <= num) {
-                        r.append(t1);
-                        i++;
-                    }
-                    break;
-                case '/':
-                    int re = t1.length() / num;
-                    r.append(t1.substring(0, re));
-                    break;
-                default: throw new NumberFormatException("Unsupported operation");
-            }
-            r.append('\"');
-            if (r.length() > 40) {
-                r.delete(40, r.length());
-                r.append(new char[]{'.', '.', '.'});
-            }
-            return new String (r);
+        r = new StringBuilder();
+        r.append('\"');
+        t1 = new StringBuffer(s1);
+        if (t1.length() > 10) t1.delete(10, t1.length());
+        switch (sign) {
+            case '*':
+                int i = 1;
+                while (i <= num) {
+                    r.append(t1);
+                    i++;
+                }
+                break;
+            case '/':
+                int re = t1.length() / num;
+                r.append(t1.substring(0, re));
+                break;
+            default: throw new NumberFormatException("Unsupported operation");
+        }
+        r.append('\"');
+        if (r.length() > 40) {
+            r.delete(40, r.length());
+            r.append(new char[]{'.', '.', '.'});
+        }
+        return new String (r);
     }
 }
